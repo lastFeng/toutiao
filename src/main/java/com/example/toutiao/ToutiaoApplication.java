b272ee9ae5b36efd417d7c6cd4003ce4b11caed0
lastFeng <563805728@qq.com>
2019-12-11 23:00:16 +0800

    其他基础文件

---
+++ src/main/java/com/example/toutiao/ToutiaoApplication.java
@@ -0,0 +1,14 @@
+package com.example.toutiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToutiaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToutiaoApplication.class, args);
+        System.out.println("Toutiao starts successfully!");
+    }
+
+}
