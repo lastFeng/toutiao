5f592e1eefcb7223054033e73b58f20d0caa470e
lastFeng <563805728@qq.com>
2019-12-12 22:43:15 +0800

    新增方法

--- src/main/java/com/example/toutiao/controller/ThymeleafIndexController.java
+++ src/main/java/com/example/toutiao/controller/ThymeleafIndexController.java
@@ -1,13 +1,18 @@
 package com.example.toutiao.controller;
 
+import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.Serializable;
+import java.util.Enumeration;
 
 /**
  * @author guowf
@@ -43,7 +48,81 @@ public class ThymeleafIndexController {
         return String.format("GID:{%s}, UID:{%d}, TYPE:{%d}, KEY:{%s}", groupId, userId, type, key);
     }
 
+    /**
+     * 请求内容的获取
+     * @param request
+     * @param response
+     * @param session
+     * @return
+     */
+    @RequestMapping("/request")
+    @ResponseBody
+    public String request(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpSession session) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        stringBuilder.append("Headers: <br>");
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            stringBuilder.append(name + ":" + request.getHeader(name) + "<br>");
+        }
+
+        stringBuilder.append("Cookie: <br>");
+        for (Cookie cookie: request.getCookies()) {
+            stringBuilder.append("Cookie:" + cookie.getName() + ":" + cookie.getValue() + "<br>");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 设置响应的Cookie与Header
+     * @param cookie
+     * @param key
+     * @param value
+     * @param response
+     * @return
+     */
+    @RequestMapping("/response")
+    @ResponseBody
+    public String response(@CookieValue(value = "Cookie", defaultValue = "123") String cookie,
+                           @RequestParam(value = "key", defaultValue = "Cookie") String key,
+                           @RequestParam(value = "value", defaultValue = "value") String value,
+                           HttpServletResponse response) {
+        response.addCookie(new Cookie(key, value));
+        response.addHeader(key, value);
+        return "guowf From Cookie: " + cookie;
+    }
+
+    /**
+     * 跳转：
+     *   301： 永久--获取本地缓存
+     *   302： 临时--从服务器中获取
+     * @param code
+     * @return
+     */
+    @RequestMapping("/redirect/{code}")
+    public RedirectView redirectView(@PathVariable("code") int code) {
+        RedirectView view = new RedirectView("/", true);
+        if (code == 301) {
+            view.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+
+        return view;
+    }
+
+    /**
+     * 302跳转
+     */
+    @RequestMapping("/redirect")
+    public String redirect() {
+        return "redirect:/";
+    }
+
+    @Data
     class User implements Serializable {
+        private static final long serialVersionUID = 7313283055058322532L;
         String name;
         int age;
         User friend;
@@ -53,30 +132,6 @@ public class ThymeleafIndexController {
 
         public User(String name, int age) {
             this.name = name;
-            this.age = age;
-        }
-
-        public User getFriend() {
-            return friend;
-        }
-
-        public void setFriend(User friend) {
-            this.friend = friend;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
             this.age = age;
         }
     }
