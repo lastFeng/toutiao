4d22e5a74e95284d353f93393e577d813a77714f
lastFeng <563805728@qq.com>
2019-12-12 14:54:38 +0800

    添加模糊查询接口

--- src/main/java/com/example/toutiao/mapper/MyMapper.java
+++ src/main/java/com/example/toutiao/mapper/MyMapper.java
@@ -3,6 +3,8 @@ package com.example.toutiao.mapper;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+import java.util.List;
+
 /**
  * @author guowf
  * @mail guowf_buaa@163.com
@@ -10,4 +12,13 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @data created in 2019/12/11 21:51
  */
 public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+
+    // FIXME: 该接口不能被扫描，否则会报错
+
+    /**
+     * 条件查询，支持模糊查询
+     * @param entity
+     * @return
+     */
+    List<T> findList(T entity);
 }
