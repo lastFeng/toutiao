58837b25fb4ac7efd6d7eaa8546cc5ac3000d72d
lastFeng <563805728@qq.com>
2019-12-13 17:36:58 +0800

    无效的全局统一异常处理，注释掉

--- src/main/java/com/example/toutiao/config/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/toutiao/config/exception/CustomExceptionHandler.java
@@ -14,30 +14,30 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @data created in 2019/12/12 22:25
  * 自定义异常句柄，但是好像没有用
  */
-@ControllerAdvice
-public class CustomExceptionHandler {
-
-    /**
-     * 统一运行时异常处理
-     * @param e
-     * @return
-     */
-    @ExceptionHandler({RuntimeException.class})
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public String customRuntimeException(RuntimeException e) {
-        return "RuntimeException: " + e.getMessage();
-    }
-
-    /**
-     * 统一检查异常处理
-     * @param e
-     * @return
-     */
-    @ExceptionHandler({Exception.class})
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public String customExceptionHandler(Exception e) {
-        return "Error: " + e.getMessage();
-    }
-}
+//@ControllerAdvice
+//public class CustomExceptionHandler {
+//
+//    /**
+//     * 统一运行时异常处理
+//     * @param e
+//     * @return
+//     */
+//    @ExceptionHandler({RuntimeException.class})
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.OK)
+//    public String customRuntimeException(RuntimeException e) {
+//        return "RuntimeException: " + e.getMessage();
+//    }
+//
+//    /**
+//     * 统一检查异常处理
+//     * @param e
+//     * @return
+//     */
+//    @ExceptionHandler({Exception.class})
+//    @ResponseBody
+//    @ResponseStatus(HttpStatus.OK)
+//    public String customExceptionHandler(Exception e) {
+//        return "Error: " + e.getMessage();
+//    }
+//}
